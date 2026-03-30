28a159d11bc346f823280e22c2364a9cad833206
Phillip Tham <phillip.tham@gmail.com>
2026-03-30 15:56:48 -0400

    adding while loop update queue, and track row, column and path

--- BFS.py
+++ BFS.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 myMaze= []
 def generateMaze(theMaze):
     with open("maze.txt", "r") as file:
@@ -14,7 +16,7 @@ def printMaze(theMaze):
         print();
 
 printMaze(myMaze)#test for printing out the maze
-
+#finds start POS for BFS
 def findStartPos(theMaze):
     for row in range(len(theMaze)):
         for col in range(len(theMaze[row])):
@@ -23,3 +25,19 @@ def findStartPos(theMaze):
             
 StartPos= findStartPos(myMaze)
 print(StartPos)
+
+#in order for BFS to work, we need a queue, a visited set, starting position, and directions(up,down,left,right)
+def bfs(theMaze,startingposition):
+    rows,cols = len(theMaze),len(theMaze[0])
+
+    queue = deque()
+    queue.append((startingposition[0],startingposition[1],[])) #current row,current col, path
+
+    visited =[]
+
+    while len(queue) > 0: #while queue is not empty run this while loop
+        CurRow, CurCol, path = queue.popleft() #removes beginning item from queue
+        if(CurRow,CurCol) in visited:
+            continue #skip this iteration if CurRow, CurCol has already been visitied
+        else:
+            visited.append(CurRow,CurCol)
