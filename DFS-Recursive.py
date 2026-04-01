767619de0f7f07f4f6e2d94c9b34a6ec9b45c806
Phillip Tham <phillip.tham@gmail.com>
2026-04-01 18:59:42 -0400

    generate maze, find start and end pos

---
+++ DFS-Recursive.py
@@ -0,0 +1,30 @@
+myMaze=[]
+mySolution=[]
+def generateMaze(theMaze):
+    with open("maze.txt","r") as file:
+        for line in file:
+            row=list(line.strip())
+            theMaze.append(row)
+        return theMaze
+myMaze=generateMaze(myMaze)
+def printMaze(theMaze):
+    for row in range(len(theMaze)):
+        for col in range(len(theMaze[row])):
+            print(theMaze[row][col], end="")
+        print(" ")
+printMaze(myMaze)
+
+def findStartPos(theMaze):
+    for row in range(len(theMaze)):
+        for col in range(len(theMaze[row])):
+            if(theMaze[row][col]=="S"):
+                return [row,col]
+startPos=findStartPos(myMaze)
+print(startPos)
+def findFinishPos(theMaze):
+    for row in range(len(theMaze)):
+        for col in range(len(theMaze[row])):
+            if(theMaze[row][col]=="F"):
+                return [row,col]
+endPos=findFinishPos(myMaze)
+print(endPos)
