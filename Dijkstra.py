df7b767eaf2bec60a5a856b7513a5ad9923c7ca4
Phillip Tham <phillip.tham@gmail.com>
2026-04-02 16:21:20 -0400

    adding generate maze and print maze helper functions

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,2 +1,15 @@
 myMaze=[]
 mySolution=[]
+def generateMaze(theMaze):
+    with open("./maze.txt","r") as file:
+        for line in file:
+            row = list(line.strip())
+            theMaze.append(row)
+        return theMaze
+def printMaze(theMaze):
+    for row in range(len(theMaze)):
+        for col in range(len(theMaze[0])):
+            print(theMaze[row][col],end="")
+        print(" ")
+myMaze=generateMaze(myMaze)
+printMaze(myMaze)
