d25862686d068c5c7d61fef78c89ac72d1885207
Phillip Tham <phillip.tham@gmail.com>
2026-04-10 23:33:55 -0400

    solved Dijkstra, updated getStartPos to get both end and start Pos, getPath reconstructs path using path grid tracker

--- Dijkstra.py
+++ Dijkstra.py
@@ -3,17 +3,20 @@ import heapq
 
 myMaze=[]
 mySolution=[]
+
 def generateMaze(theMaze):
     with open("./maze.txt","r") as file:
         for line in file:
             row = list(line.strip())
             theMaze.append(row)
         return theMaze
+    
 def printMaze(theMaze):
     for row in range(len(theMaze)):
         for col in range(len(theMaze[0])):
             print(theMaze[row][col],end="")
         print(" ")
+
 myMaze=generateMaze(myMaze)
 printMaze(myMaze)
 
@@ -30,11 +33,16 @@ def scatterWeights(theMaze):
                 theMaze[row][col]=randomWeight
     return theMaze
 
-def findStartPos(theMaze):
+def findStartEndPos(theMaze):
+    startPos= []
+    endPos=[]
     for row in range(len(theMaze)):
         for col in range(len(theMaze[0])):
             if(theMaze[row][col] =="S"):
-                return [row,col]
+                startPos= [row,col]
+            if(theMaze[row][col] =="F"):
+                endPos=[row,col]
+    return startPos,endPos
 #calculates cost of cell value passed
 def getCost(value):
     costs= {
@@ -44,12 +52,71 @@ def getCost(value):
     }
     return costs.get(value,None)
 
+def getGrids(theMaze):
+    theGrid=[]
+    prevGrid=[]
+    for row in range(len(theMaze)):
+        newRow=[]
+        newPrevRow=[]
+        for col in range(len(theMaze[0])):
+            newRow.append(float("inf"))
+            newPrevRow.append(None)
+        theGrid.append(newRow)
+        prevGrid.append(newPrevRow)
+    for row in range(len(theMaze)):
+        for col in range(len(theMaze[0])):
+            if(theMaze[row][col]=="1"):
+                theGrid[row][col]=-1
+    return theGrid,prevGrid
 
 def dijkstra(theMaze,startPos):
-    currentCost=0
+    startCost=0
     heapqueue=[]
-    heapq.heappush(heapqueue,(currentCost,startPos[0],startPos[1]))
+    heapq.heappush(heapqueue,(startCost,startPos[0],startPos[1]))
+    distGrid,prevGrid=getGrids(theMaze)
+    #set up distGrid
+    distGrid[startPos[0]][startPos[1]]=0
+    #printMaze(distGrid)
+    while(heapqueue):
+        currentCost,curRow,curCol=heapq.heappop(heapqueue)
+        if(theMaze[curRow][curCol]=="F"):
+            return currentCost,prevGrid
+        #up [-1,0] left[0,-1] down[1,0] right[0,1]
+        for deltaRow,deltaCol in ([-1,0],[0,-1],[1,0],[0,1]):
+            tempRow=curRow+deltaRow
+            tempCol=curCol+deltaCol
+            #boundary check
+            if(0 <= tempRow <len(theMaze) and 0<= tempCol < len(theMaze[0])):
+                if(theMaze[tempRow][tempCol]=="F"):
+                    prevGrid[tempRow][tempCol]=(curRow,curCol)
+                    return currentCost, prevGrid
+                if(theMaze[tempRow][tempCol]=="0"):
+                    tempCost=currentCost+getCost(0)
+                elif(theMaze[tempRow][tempCol]=="3"):
+                    tempCost=currentCost+getCost(3)
+                elif(theMaze[tempRow][tempCol]=="4"):
+                    tempCost=currentCost+getCost(4)
+                else:
+                    continue
+                if tempCost < distGrid[tempRow][tempCol]:
+                    distGrid[tempRow][tempCol]=tempCost
+                    prevGrid[tempRow][tempCol]=(curRow,curCol)
+                    heapq.heappush(heapqueue,(tempCost,tempRow,tempCol))
+            
+def getPath(pathGrid,endPos):
+    cur=endPos
+    path =[]
+    while cur is not None:
+        path.append(cur)
+        cur=pathGrid[cur[0]][cur[1]]
+    path.reverse()
+    return path
 
 #update maze to get scattered weights
 myMaze=scatterWeights(myMaze)
 printMaze(myMaze)
+startPos,endPos=findStartEndPos(myMaze)
+shortestCost,pathGrid=dijkstra(myMaze,startPos)
+print("===================================================")
+myPath=getPath(pathGrid,endPos)
+print(myPath)
